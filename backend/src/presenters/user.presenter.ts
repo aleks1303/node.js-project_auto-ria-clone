166147b5384ea09ea6ad3f7ef978ac765bbeb559
aleks1303 <aleksbulda13@gmail.com>
2026-01-16 16:57:40 +0200

    init

--- backend/src/presenters/user.presenter.ts
+++ backend/src/presenters/user.presenter.ts
@@ -37,8 +37,46 @@ class UserPresenter {
             // Використовуємо стрілочну функцію, щоб не втратити контекст 'this'
             data: entities.map((entity) => this.toPublicResDto(entity)),
             total,
-            query,
+            ...query,
         };
     }
 }
 export const userPresenter = new UserPresenter();
+
+// import { configs } from "../configs/config";
+// import {
+//     IUser,
+//     IUserListQuery,
+//     IUserListResponse,
+//     IUserResponse,
+// } from "../interfaces/user.interface";
+//
+// class UserPresenter {
+//     public toPublicResDto(entity: IUser): IUserResponse {
+//         return {
+//             _id: entity._id,
+//             name: entity.name,
+//             email: entity.email,
+//             age: entity.age,
+//             role: entity.role,
+//             avatar: entity.avatar
+//                 ? `${configs.AWS_S3_ENDPOINT}/${entity.avatar}`
+//                 : null,
+//             isDeleted: entity.isDeleted,
+//             isVerified: entity.isVerified,
+//         };
+//     }
+//
+//     public toListResDto(
+//         entities: IUser[],
+//         total: number,
+//         query: IUserListQuery,
+//     ): IUserListResponse {
+//         return {
+//             data: entities.map(this.toPublicResDto),
+//             total,
+//             ...query,
+//         };
+//     }
+// }
+// export const userPresenter = new UserPresenter();
