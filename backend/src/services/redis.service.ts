e8a4c2f52c7f3819f22c38f20d2643c53fecea56
aleks1303 <aleksbulda13@gmail.com>
2026-01-23 20:55:05 +0200

    init

--- backend/src/services/redis.service.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "redis";
-
-export const redisClient = createClient({ url: "redis://localhost:6379" });
-redisClient.on("error", (err) => console.log("Redis Client Error", err));
-await redisClient.connect();
